497ec3d6ce9e9694816938cce15735e03cf8deda
mklau <matthewklau@fas.harvard.edu>
2020-11-20 14:12:55 -0500

    finishes talk

--- make_talk.sh
+++ make_talk.sh
@@ -7,6 +7,9 @@ pdflatex \\nonstopmode\\input talk.tex
 mv talk.pdf talk_talk.pdf
 echo \\setbeameroption{show only notes} > .mk.out
 pdflatex \\nonstopmode\\input talk.tex
+mv talk.pdf notes.pdf
+echo \\setbeameroption{notes} > .mk.out
+pdflatex \\nonstopmode\\input talk.tex
 mv talk.pdf talk_notes.pdf
 
 pdftotext talk_talk.pdf talk_talk.txt
